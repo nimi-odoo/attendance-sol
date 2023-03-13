26e7c66ff76189de8777fbb64c3ac3e82ad2fdab
nimi-odoo <107050135+nimi-odoo@users.noreply.github.com>
2023-03-13 19:17:49 -0400

    Initial commit

---
+++ static/src/js/kioskconfirm.js
@@ -0,0 +1,30 @@
+odoo.define('attendance.kioskconfirm', function (require) {
+    "use strict";
+
+    const core = require('web.core');
+    // const ajax = require('web.ajax');
+    const QWeb = core.qweb;
+    // ajax.loadXML('/attendance/static/src/xml/attendance.xml', QWeb);
+
+    let kioskConfirm = require('hr_attendance.kiosk_confirm');
+
+    kioskConfirm.include({
+        init: function (parent, action) {
+            this._super.apply(this, arguments);
+            this.next_action = 'attendance.hr_attendance_action_kiosk_mode_inherit';
+            this.sale_order_line_id = action.sale_order_line_id;
+            this.sol_name = action.sol_name;
+            this.sol_state = action.sol_state;
+            // this.employee_hours_today = field_utils.format.float_time(action.employee_hours_today);
+        },
+        start: function () {
+            var self = this;
+            self.$el.html(QWeb.render("HrAttendanceKioskConfirmInherit", {widget: self}));
+            self.start_clock();
+            return self._super.apply(this, arguments);
+        },
+        
+    });
+    
+    core.action_registry.add('hr_attendance_kiosk_confirm', kioskConfirm);
+})
---
+++ static/src/js/kioskmode.js
@@ -0,0 +1,17 @@
+odoo.define('attendance.kioskmode', function (require){
+    "use strict";
+    // var core = require('web.core');
+    // require original module JS
+    let kiosk = require('hr_attendance.kiosk_mode');
+    
+    // Extend widget
+    kiosk.include({
+        events: {
+            "click .o_hr_attendance_button_employees": function() {
+                this.do_action('attendance.hr_employee_attendance_action_kanban_inherit', {
+                    additional_context: {'no_group_by': true},
+                });
+            },
+        }
+        },);
+})
