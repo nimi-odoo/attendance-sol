bf00fbe4f8dc3150c42e5dbc2deed94b2cd156ed
nimi-odoo <nimi@odoo.com>
2023-03-14 15:17:44 +0000

    [DEL] Moved module files into attendance-sol

--- models/hr_attendance.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class HrAttendance(models.Model):
-    _inherit = 'hr.attendance'
-
-    sale_order_line_id = fields.Many2one('sale.order.line', string="Sale Order Line", required=True, ondelete='cascade', index=True)
-
